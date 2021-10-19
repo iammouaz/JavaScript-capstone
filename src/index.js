a36061aecaf6354250aa6cb84db1e6b94a131742
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-10-19 18:10:19 +0300

    Create list method from api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-<<<<<<< Updated upstream
-import _ from 'lodash';
-import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
-=======
 import './style.css';
 import * as myModule from './api';
 
@@ -21,4 +5,3 @@ const main = document.getElementById('listedShow');
 const Shows = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '18'];
 
 myModule.loadShows(Shows, main);
->>>>>>> Stashed changes
